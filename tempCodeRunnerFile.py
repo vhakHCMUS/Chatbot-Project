6b44619b495755cf2fbaac41ebe186a9b6b0946f
vhakHCMUS <22127201@student.hcmus.edu.vn>
2025-01-03 15:19:39 +0700

    Unnecessary files deleted

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-# Load the model and tokenizer
-model_name = "meta-math/MetaMath-Mistral-7B"  # Assuming your model is a version of this
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained(checkpoint_dir)
